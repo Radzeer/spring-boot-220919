c122fdcf5da52870c35a35556946aa1d9bd89b4e
Radzeer <hollo.attila.82@gmail.com>
2022-09-19 11:53:17 +0200

    Service, Lombok, DI

--- src/main/java/training/employees/HelloController.java
+++ src/main/java/training/employees/HelloController.java
@@ -1,5 +1,7 @@
 package training.employees;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,11 +10,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.time.LocalDateTime;
 
 @Controller
+@AllArgsConstructor
 public class HelloController {
+
+
+    private HelloService helloService;
+
+    //Construktor injection
+   /* public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }*/
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     @ResponseBody // a visszatérési értéket írja a http válaszba.
     public String sayHello(){
-        return "Hello! " + LocalDateTime.now();
+        return helloService.sayHello();
     }
 }
---
+++ src/main/java/training/employees/HelloService.java
@@ -0,0 +1,12 @@
+package training.employees;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+@Service
+public class HelloService {
+
+    public String sayHello(){
+        return "Hello from Service! " + LocalDateTime.now();
+    }
+}
