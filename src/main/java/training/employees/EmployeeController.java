0a11033cdebbe185b2b575186ee27cb188efa31d
Radzeer <hollo.attila.82@gmail.com>
2022-09-20 16:02:25 +0200

    created status code

--- src/main/java/training/employees/EmployeeController.java
+++ src/main/java/training/employees/EmployeeController.java
@@ -1,8 +1,11 @@
 package training.employees;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,10 +25,13 @@ public class EmployeeController {
     public EmployeeDetailsDto findEmployeeById(@PathVariable("id") long id){
         return service.findEmployeeById(id);
     }
-
     @PostMapping
-    public EmployeeDetailsDto createEmployee(@RequestBody CreateEmployeeCommand command){
-        return service.createEmployee(command);
+    public ResponseEntity<EmployeeDetailsDto> createEmployee(@RequestBody CreateEmployeeCommand command,
+                                                             UriComponentsBuilder uri) {
+        var employee = service.createEmployee(command);
+        return ResponseEntity
+                .created(uri.path("/api/employees/{id}").buildAndExpand(employee.getId()).toUri())
+                .body(employee);
     }
 
     @PutMapping("{id}")
