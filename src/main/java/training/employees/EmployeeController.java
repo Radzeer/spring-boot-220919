a508e9e3803eac4a6f20dc9e85616c483ece6533
Radzeer <hollo.attila.82@gmail.com>
2022-09-20 11:14:19 +0200

    rest 1

---
+++ src/main/java/training/employees/EmployeeController.java
@@ -0,0 +1,20 @@
+package training.employees;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @GetMapping
+    public List<Employee> listEmployees(){
+        return List.of(new Employee(1L,"John Doe"),
+                new Employee(2L,"Jane Doe"));
+    }
+}
